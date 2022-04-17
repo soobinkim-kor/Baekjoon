7ed35dffde410174c33f960665825c1ecdb9af0b
kim <shb03207@naver.com>
2022-04-17 18:21:53 +0900

    2022/04/17

--- src/baekjoon/silver1/baek1149.java
+++ src/baekjoon/silver1/baek1149.java
@@ -1,93 +1,31 @@
 package baekjoon.silver1;
 
 import java.io.*;
-import java.util.Deque;
-import java.util.LinkedList;
-
-import static java.lang.Math.min;
 
 public class baek1149 {
-    static House[] houseList;
-    static String[] colors={"Red","Green","Blue"};
-    static int price=0;
+    static int houses;
+    static int[][] dp;
     public void answer()throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int houses= Integer.parseInt(br.readLine());
-        houseList = new House[houses];
+        houses= Integer.parseInt(br.readLine());
 
-        for(int i=0;i<houses;i++){
+        dp=new int[houses+1][3];
+
+        for(int i=1;i<=houses;i++){
             String[] info = br.readLine().split(" ");
-            House house= new House();
-            house.setToRed(Integer.parseInt(info[0]));
-            house.setToGreen(Integer.parseInt(info[1]));
-            house.setToBlue(Integer.parseInt(info[2]));
-            houseList[i]=house;
-        }
+            int toRed = Integer.parseInt(info[0]);
+            int toGreen = Integer.parseInt(info[1]);
+            int toBlue = Integer.parseInt(info[2]);
 
-        for(House data:houseList){
-            System.out.println("집의 색: "+data.getColor()+" 빨강비용: "+data.getToRed()+" 초록비용: "+data.getToGreen()+" 파랑비용: "+data.getToBlue());
+            dp[i][0]=Math.min(dp[i-1][1],dp[i-1][2])+toRed;
+            dp[i][1]=Math.min(dp[i-1][0],dp[i-1][2])+toGreen;
+            dp[i][2]=Math.min(dp[i-1][0],dp[i-1][1])+toBlue;
         }
-
-        for(int i=0;i<3;i++){
-            price=min(price,bfs(colors[i]));
-        }
-
-        System.out.println(price);
+        wr.write(Math.min(dp[houses][0],Math.min(dp[houses][1],dp[houses][2]))+"");
+        wr.close();
     }
 
-    public static int bfs(String firstHouseColor){
-        Deque<House> deque=new LinkedList<>();
-        House house = houseList[0];
-        house.setColor(firstHouseColor);
-
-        deque.add(house);
-        while(!deque.isEmpty()){
-            House polledHouse=deque.poll();
-
-        }
-        return 0;
-    }
-
-    public class House{
-
-        int toRed;
-        int toBlue;
-        int toGreen;
-        String color;
-
-
-        public String getColor(){
-            return this.color;
-        }
-
-        public void setColor(String color){
-            this.color=color;
-        }
-
-        public int getToRed(){
-            return this.toRed;
-        }
-
-        private void setToRed(Integer price){
-            this.toRed=price;
-        }
-
-        public int getToBlue(){
-            return this.toBlue;
-        }
-
-        private void setToBlue(Integer price){
-            this.toBlue=price;
-        }
-
-        public int getToGreen(){
-            return this.toGreen;
-        }
-        private void setToGreen(Integer price){
-            this.toGreen=price;
-        }
-    }
 }

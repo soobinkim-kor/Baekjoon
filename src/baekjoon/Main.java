080f0d80bdd2368a3410f9943853423a739c15eb
kim <shb03207@naver.com>
2022-04-22 14:48:51 +0900

    2022/04/22

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -2,16 +2,19 @@ package baekjoon;
 
 import baekjoon.silver1.*;
 import baekjoon.silver2.*;
-import baekjoon.silver4.*;
 import baekjoon.silver3.*;
+import baekjoon.silver4.*;
+
+
+import baekjoon.gold3.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        baek1931 baek1931 = new baek1931();
+        baek1005 baek1005 = new baek1005();
 
-        baek1931.answer();
+        baek1005.answer();
     }
 }
 
--- src/baekjoon/gold3/baek1005.java
+++ src/baekjoon/gold3/baek1005.java
@@ -1,5 +1,50 @@
 package baekjoon.gold3;
 
-public class baek1005 {
+import java.io.*;
 
+public class baek1005 {
+    static int houses;
+    static int rules;
+    static int[] prices;
+    static int[][] rule;
+    static int target;
+    public static void answer() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testcase= Integer.parseInt(br.readLine());      //테스트 케이스
+        for(int i=0;i<testcase;i++){
+            String[] info = br.readLine().split(" ");
+            houses = Integer.parseInt(info[0]);         // 건물 갯수
+            rules = Integer.parseInt(info[1]);          // 규칙 갯수
+            prices = new int[houses+1];                 // 건물 건설 비용 ( 0은 제외. 인덱스 1 = 1번 건물 )
+            String[] pricesinfo = br.readLine().split(" ");
+            for(int j=0;j<houses;j++){
+                prices[j+1]=Integer.parseInt(pricesinfo[j]);
+            }
+
+            rule = new int[rules][2];
+
+
+            for(int k=0;k<rules;k++){
+                String ruleinfo[] = br.readLine().split(" ");
+                rule[k]=new int[]{Integer.parseInt(ruleinfo[0]),Integer.parseInt(ruleinfo[1])};
+            }
+
+            target=Integer.parseInt(br.readLine());
+
+            System.out.println(houses+" "+rules);
+
+            for(int data:prices){
+                System.out.print(data+" ");
+            }
+            System.out.println(" ");
+            for(int[] data:rule){
+                System.out.println(data[0]+" "+data[1]);
+            }
+
+            System.out.println(target);
+        }
+    }
 }
